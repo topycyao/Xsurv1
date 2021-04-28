4aac3c1024e1868a04d30a098fd6d5a0f5a9d5c5
topycyao <69061984+topycyao@users.noreply.github.com>
2021-04-28 14:27:38 -0400

    update

--- R/sim_surv_tree.R
+++ R/sim_surv_tree.R
@@ -5,11 +5,14 @@
 #' @param x_data X data set
 #' @param y_data Y data set
 
-   sim_surv_tree<-function(model,x_data,y_data,top_nc=NULL){
+   sim_surv_tree<-function(model,x_data,y_data,top_nc=NULL,maxdp=NULL,cp=NULL){
      y_data=as.data.frame(y_data)
      cnames<-colnames(x_data)
      xdata=data.matrix(x_data)
      if(is.null(top_nc)){top_nc=3}
+     if(is.null(maxdp)){maxdp=3}
+
+
      imp<-SHAPforxgboost::shap.values(model,xdata)$mean_shap_score
      top_name<-names(imp)
 
@@ -21,7 +24,7 @@
      x_tree<-as.data.frame(x_data[,idx])
 
      yt<-survival::Surv(y_data$time,y_data$status)
-     fit<-rpart::rpart(yt~.,data=x_tree,control = rpart.control(maxdepth=4))
+     fit<-rpart::rpart(yt~.,data=x_tree,control = rpart.control(maxdepth=maxdp,cp=cp))
      tfit<-partykit::as.party(fit)
      tfit1<-partykit::as.party(fit)
      plot(tfit1)
